d8a49dbff93b65670d72d9077bc99d37e4c4c1a7
casey.Q.Wood <caseywood252@gmail.com>
2021-08-14 19:17:19 -0700

    base hero with h1/h2, background greyscale and some junk file clean up

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+export default function Hero() {
+
+  return (
+    <div className='hero__container'>
+      <h1>
+        This is Casey Wood!
+      </h1>
+      <h2>Casey Wood is currently making his React based resume</h2>
+    </div>
+  )
+}
