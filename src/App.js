138e198fed04ab0a0cfaa72646a2eedfb1e919ee
casey.Q.Wood <caseywood252@gmail.com>
2021-08-14 19:56:14 -0700

    Sass install, base header size style and placement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 import Hero from './Components/Hero';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Hero/>
     </div>
   );
