0c50e9db8365d181c8640ebd28d93f1a4463ef73
casey.Q.Wood <caseywood252@gmail.com>
2021-08-16 19:17:14 -0700

    clean up code for nav hide on scroll

--- src/App.js
+++ src/App.js
@@ -3,24 +3,12 @@ import Hero from './Components/Hero';
 import Header from './Components/Header';
 import About from './Components/About';
 import Carousel from './Components/primitveComponents/Carousel';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [visable, setVisable] = useState(true)
-
-  const top = window.pageYOffset
-  console.log(top)
-  
-
-  useEffect(() => {
-    window.onscroll = () => {
-      window.pageYOffset > 5 ? setVisable(false) : setVisable(true)
-    }
-  })
 
   return (
     <div className="App">
-      <Header visable={visable}/>
+      <Header/>
       <Hero/>
       <About/>
       <Carousel/>
