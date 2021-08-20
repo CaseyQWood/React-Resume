10d557b9d4119ee3f9268f1089fd30a57f8408c5
casey.Q.Wood <caseywood252@gmail.com>
2021-08-19 18:22:13 -0700

    name animation with single run of animation with nav zindex update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,9 +5,47 @@ export default function Hero() {
 
   return (
     <div id='home__page' className='hero__container'>
-      <h1>
+
+
+      <div className='main'>
+      
+      <div id="animation-5" class="animation">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          <defs>
+            <clipPath id="mask-bottom-right">
+              <rect class="clip-path" x="70%" y="0" width="30%" height="100%"/>
+            </clipPath>
+            <clipPath id="mask-top">
+              <rect class="clip-path" x="0" y="0" width="100%" height="100%"/>
+            </clipPath>
+            <clipPath id="mask-bottom-left">
+              <rect class="clip-path" x="0" y="0" width="30%" height="100%"/>
+            </clipPath>
+          </defs>
+          
+          <line class="bottom-right" x1="70%" y1="100%" x2="100%" y2="100%"/>
+          <line class="right" x1="100%" y1="0" x2="100%" y2="100%"/>
+          <line class="top" x1="0" y1="0" x2="100%" y2="0"/>
+          <line class="left" x1="0" y1="0" x2="0" y2="100%"/>
+          <line class="bottom-left" x1="0" y1="100%" x2="30%" y2="100%"/>
+        </svg>
+        <div class="white bold arimo mask">
+          <div>Casey Wood</div>
+        </div>
+        <div class="white mask">
+          <div>Web Developer</div>
+        </div>
+      </div>
+      </div>
+
+      {/* <h1>
         This is Casey Wood!
-      </h1>
+      </h1> */}
+
+
+
+
+
       <h2>Addicted to learning and pushing to improve. I am some one who is always eager to pick up a new skill and always willing to adapt to challenges. In my spare time I typically spend my time tinkering on projects, playing warhammer or video games</h2>
     </div>
   )
