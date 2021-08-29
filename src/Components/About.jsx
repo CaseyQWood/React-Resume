6f07b6b399586f31390b12d506990c2db0f2009f
zoeElizabeth <zoe.gelech@gmail.com>
2021-08-28 18:01:01 -0700

    setting up about skeleton

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div id='about__page' className='about__container'>
 
-      <h2>About Me:</h2>
+      <h2>About Me</h2>
 
       <div className='about__body'>
 
@@ -15,9 +15,26 @@ export default function About() {
       </div>
 
       <div className='about__description'>
-        <p>
-        Addicted to learning and pushing to improve. I am some one who is always eager to pick up a new skill and always willing to adapt to challenges. In my spare time I typically spend my time tinkering on projects, playing warhammer or video games 
-        </p>
+          <p>
+            Addicted to learning and pushing to improve. I am some one who is always eager to pick up a new skill and always willing to adapt to challenges. In my spare time I typically spend my time tinkering on projects, playing warhammer or video games 
+          </p>
+        <h3>Contact Info</h3>
+        <div class= "about__contact">
+          <ul>
+            <li><a href="mailto:name@email.com">name@email.com</a></li>
+            <li>Vancouver, Bc</li>
+          </ul>
+
+          <div class="middle">
+            <a class="social-button" href="#">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a class="social-button" href="#">
+              <i class="fab fa-wordpress"></i>
+            </a>
+          </div>
+
+        </div>
       </div>
 
       </div>
